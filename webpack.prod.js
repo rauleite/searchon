f8bd9854a1ab4a0eee0035928dcdfac68abf8bea
Raul Leite <raul@mac.local>
2020-08-23 11:57:18 -0300

    release 1.0

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,28 +1,46 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
   // devtool: 'source-map',
   // devtool: 'cheap-inline-module-source-map',
   mode: 'production',
+  // externals: {
+  //   // child_process: 'child_process',
+  //   // fs: 'fs',
+  //   // readline: 'readline',
+  // },
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+
   optimization: {
     // should be included?
     // optimization: {
-  //   usedExports: true,
-  // },
+    // usedExports: true,
+    // },
+    // concatenateModules: false,
 
-    // minimize: true,
-    // minimizer: [
-    //   new TerserPlugin({
-    //     test: /\.js(\?.*)?$/i,
-    //     sourceMap: true,
-    //     terserOptions: {
-    //       ecma: 2020,
+    minimize: true,
+    minimizer: [
+      // new ClosurePlugin({ mode: 'AGGRESSIVE_BUNDLE' }, {
+      // }),
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        cache: true,
+        parallel: true,
 
-    //     },
-    //   }),
-    // ],
+        // sourceMap: true,
+        // exclude: /node_modules/,
+        terserOptions: {
+          ecma: 2020,
+
+        },
+      }),
+    ],
   },
+  plugins: [
+  ],
 });
